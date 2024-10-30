ef636ace97834081b6b87cd0e7f77dbd4cc46cee
piro0919 <piro.haniwa@gmail.com>
2024-10-30 22:48:50 +0900

    build: maintain environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
