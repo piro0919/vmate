ea6ab9fcafaddbd21709a332e08b157a1c79bd08
piro0919 <piro.haniwa@gmail.com>
2024-11-15 15:35:45 +0900

    build: downgrade react and next

--- apps/liver/next.config.mjs
+++ apps/liver/next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
