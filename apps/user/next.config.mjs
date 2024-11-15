4d610ef9cc1b3b6879a002eefd53c608d3df0ac8
piro0919 <piro.haniwa@gmail.com>
2024-11-15 15:44:18 +0900

    build: add jiti for env types

--- apps/user/next.config.mjs
+++ apps/user/next.config.mjs
@@ -1,11 +1,12 @@
+import { fileURLToPath } from "node:url";
+import { createJiti } from "jiti";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+await jiti.import("./src/env", { default: true });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-
   experimental: {
     typedRoutes: true,
   },
