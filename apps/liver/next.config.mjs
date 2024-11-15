57bfbb9ce5f6b982408eafc5e23e8daeb7b80c59
piro0919 <piro.haniwa@gmail.com>
2024-11-15 16:12:08 +0900

    build: enable eslint before production build

--- apps/liver/next.config.mjs
+++ apps/liver/next.config.mjs
@@ -7,11 +7,6 @@ await jiti.import("./src/env", { default: true });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     typedRoutes: true,
   },
